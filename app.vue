4916269f21f5a43bdf82088e9920836819a92fa2
Yoji Kojio <yoji.kojio@gmail.com>
2023-03-29 15:18:01 -0300

    first commit

---
+++ app.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="app">
+    <CardContainer>
+      Query params: {{ route.query }}
+    </CardContainer>
+  </div>
+</template>
+
+<script setup>
+const route = useRoute()
+</script>
+
+<style>
+.app {
+  display: flex;
+  justify-content: center;
+}
+</style>
---
+++ components/CardContainer.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="card-container">
+    <slot />
+  </div>
+</template>
+
+<style>
+.card-container {
+  background-color: #fff;
+  padding: 32px;
+  border-radius: 4px;
+  border: 1px solid #262626;
+  max-width: 400px;
+}
+</style>
